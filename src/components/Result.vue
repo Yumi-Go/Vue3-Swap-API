aec4a406255ee16427ed536b21f835c78e1ce31f
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-04-17 12:55:17 +0100

    tailwind setting

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -6,8 +6,20 @@
 <template>
 
 <div>
+    <table>
+        <thead>
+            <tr>
+                <th></th> <!-- dynamic column name rendering here -->
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td></td> <!-- dynamic value rendering here -->
+            </tr>
+        </tbody>
+    </table>
 
-    
+
 </div>
 
 

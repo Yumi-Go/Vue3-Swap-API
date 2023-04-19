c8892f440b7abfa25e583cbf9862117d7bb0f1c3
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-04-19 01:18:06 +0100

    useAxios & local storage fix

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -7,32 +7,27 @@ import { useLocalStorage, StorageSerializers } from '@vueuse/core';
 const getAllUsers = useLocalStorage("users", null, { serializer: StorageSerializers.object });
 const getAllPlanets = useLocalStorage("planets", null, { serializer: StorageSerializers.object });
 
-useAxiosUsers();
-useAxiosPlanets();
+const { userItems, saveUsersToStorage } = useAxiosUsers();
+const { planetItems, savePlanetsToStorage } = useAxiosPlanets();
 
-// console.log(getAllUsers.value);
-// console.log(getAllPlanets.value);
+saveUsersToStorage();
+savePlanetsToStorage();
 
-// const columnNames = ['Name', 'Height', 'Mass', 'Created', 'Edited', 'Planet Name'];
+console.log(getAllUsers.value);
+console.log(getAllPlanets.value);
 
-// function columnContents(index) {
 
-//     const userRow = [];
-//     const lowercaseColNames = columnNames.map(name => name.toLowerCase());
-//     const user = Object.values(getAllUsers.value)[index];
-//     console.log(user);
-    
-//     for (const [key, value] of Object.entries(user)) {
-//         if (lowercaseColNames.includes(key)) {
-//             userRow.push(value);
-//         }
-//     }
-
-//     console.log("userRow: ", userRow);
-//     return userRow;
-
-// }
-
+function columnContents(index) {
+    const userRow = [];
+    const lowercaseColNames = userItems.map(name => name.toLowerCase());
+    const user = getAllUsers.value[index];
+    for (const [key, value] of Object.entries(user)) {
+        if (lowercaseColNames.includes(key)) {
+            userRow.push(value);
+        }
+    }
+    return userRow;
+}
 
 </script>
 
@@ -41,10 +36,10 @@ useAxiosPlanets();
 
 <div>
 
-    <!-- <table class="w-[600px] table-auto border-solid border-2">
+    <table class="w-[600px] table-auto border-solid border-2">
         <thead class="border-solid border-2">
             <tr class="border-solid border-2">
-                <th v-for="column in columnNames" class="border-solid border-2">{{ column }}</th>
+                <th v-for="column in userItems" class="border-solid border-2">{{ column }}</th>
             </tr>
         </thead>
         <tbody class="border-solid border-2">
@@ -52,7 +47,7 @@ useAxiosPlanets();
                 <td v-for="cell in columnContents(i-1)" class="border-solid border-2">{{ cell }}</td>
             </tr>
         </tbody>
-    </table> -->
+    </table>
 
 
 </div>
--- src/composables/useAxios.js
+++ src/composables/useAxios.js
@@ -1,4 +1,3 @@
-import { ref } from 'vue'
 import axios from 'axios'
 import { useLocalStorage, StorageSerializers } from '@vueuse/core';
 
@@ -8,51 +7,122 @@ const getAllUsers = useLocalStorage("users", null, { serializer: StorageSerializ
 const getAllPlanets = useLocalStorage("planets", null, { serializer: StorageSerializers.object });
 
 
-export async function useAxiosUsers() {
-    
-    const responsesList = [];
-    for (let userCount = 1; userCount < 100; userCount++) {
-        try {
-            const response = await axios.get(`https://swapi.dev/api/people/${userCount}`);
-            if (response.status) {
-                responsesList.push(response);
-            }
-        } catch (error) {
-            console.error(error);
-        }   
-    }
-    responsesList.forEach(res => console.log(res));
-    console.log("users length: ", responsesList.length);
-    return responsesList;
+export function useAxiosUsers() {
 
+    const userItems = ['name', 'height', 'mass', 'created', 'edited', 'planet name'];
+
+    async function getValidUsers() {
+        const responsesList = [];
+        for (let userCount = 1; userCount < 100; userCount++) {
+            try {
+                const response = await axios
+                .get(`https://swapi.dev/api/people/${userCount}`);
+                if (response.status) {
+                    responsesList.push(response);
+                }
+            } catch (error) {
+                console.error(error);
+            }   
+        }
+
+        return responsesList;
+    }
+
+    async function getPlanetName(planetURL) {
+        let result = "";
+        await axios
+        .get(planetURL)
+        .then((response) => {
+            result = response.data.name;
+        });
+        return result;
+    }
+
+    async function refineUsersDB() {
+        const allUsersRefinedData = [];
+        const usersPromise = Promise.resolve(getValidUsers());
+        await usersPromise
+        .then((user) => {
+            const userRawData = Object.values(user);
+            userRawData.forEach(el => {
+                const userRefinedData = [];
+                for (const [key, value] of Object.entries(el.data)) {
+                    if (userItems.includes(key)) {
+                        userRefinedData.push(value);
+                    }
+                    if (key === "homeworld") {
+                        getPlanetName(value);
+                        const planetNamesPromise = Promise.resolve(getPlanetName(value));
+                        planetNamesPromise.then((planetName) => {
+                            userRefinedData.push(planetName);
+                        });
+                    }
+                }
+                allUsersRefinedData.push(userRefinedData);
+            });
+        });
+        return allUsersRefinedData;
+    }
+
+    async function saveUsersToStorage() {
+        if (getAllUsers.value.length < 1) {
+            Object.values(await refineUsersDB()).forEach(user => {
+                saveUsers.value.push(user);
+            });
+            saveUsers.value.push(await refineUsersDB());
+        }
+    }
+
+    return { userItems, saveUsersToStorage }
 }
 
-export async function useAxiosPlanets() {
-    const responsesList = [];
-    for (let planetCount = 1; planetCount < 100; planetCount++) {
-        try {
-            const response = await axios.get(`https://swapi.dev/api/planets/${planetCount}`);
-            if (response.status) {
-                responsesList.push(response);
-            }
-        } catch (error) {
-            console.error(error);
-        }   
+
+export function useAxiosPlanets() {
+
+    const planetItems = ['name', 'diameter', 'climate', 'population'];
+
+    async function getValidPlanets() {
+        const responsesList = [];
+        for (let planetCount = 1; planetCount < 100; planetCount++) {
+            try {
+                const response = await axios
+                .get(`https://swapi.dev/api/planets/${planetCount}`);
+                if (response.status) {
+                    responsesList.push(response);
+                }
+            } catch (error) {
+                console.error(error);
+            }   
+        }
+        return responsesList;
     }
-    responsesList.forEach(res => console.log(res));
-    console.log("planets length: ", responsesList.length);
-    return responsesList;
+
+    async function refinePlanetsDB() {
+        const allPlanetsRefinedData = [];
+        const planetsPromise = Promise.resolve(getValidPlanets());
+        await planetsPromise
+        .then((planet) => {
+            const planetRawData = Object.values(planet);
+            planetRawData.forEach(el => {
+                const planetRefinedData = [];
+                for (const [key, value] of Object.entries(el.data)) {
+                    if (planetItems.includes(key)) {
+                        planetRefinedData.push(value);
+                    }
+                }
+                allPlanetsRefinedData.push(planetRefinedData);
+            });
+        });
+        return allPlanetsRefinedData;
+    }
+
+    async function savePlanetsToStorage() {
+        
+        if (getAllPlanets.value.length < 1) {
+            Object.values(await refinePlanetsDB()).forEach(planet => {
+                savePlanets.value.push(planet);
+            });
+        }
+    }
+    return { planetItems, savePlanetsToStorage }
 }
-
-
-
-
-
-// export function useAxiosPlanets() {
-//     axios.get('https://swapi.dev/api/planets/').then((response) => {
-//         if (getAllPlanets.value.length == 0) {
-//             response.data.results.forEach(planet => savePlanets.value.push(planet));
-
-//         }
-//     });
-// }

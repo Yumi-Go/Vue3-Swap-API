0eab9a7727919ae19d4f0d2f71db087b9e6d0e8b
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-04-20 16:52:01 +0100

    getting data from api

--- src/composables/useAxios.js
+++ src/composables/useAxios.js
@@ -1,4 +1,3 @@
-import { ref } from 'vue';
 import axios from 'axios'
 import { useLocalStorage, StorageSerializers } from '@vueuse/core';
 
@@ -7,29 +6,26 @@ const savePlanets = useLocalStorage('planets', []);
 const getAllUsers = useLocalStorage("users", null, { serializer: StorageSerializers.object });
 const getAllPlanets = useLocalStorage("planets", null, { serializer: StorageSerializers.object });
 
-// request data for every user at once
-
 export function useAxiosUsers() {
 
     const userItems = ['name', 'height', 'mass', 'created', 'edited', 'planet name'];
 
-    async function getEachPageUsers() {
-        const responsesList = [];
-        for (let pageCount = 1; pageCount < 10; pageCount++) {
-            try {
+    async function getAllPagesUsers() {
+        try {
+            const responsesList = [];
+            let pageCount = 1;
+            let isNextPageExists = true;
+            while (isNextPageExists) {
                 const response = await axios.get(`https://swapi.dev/api/people/?page=${pageCount}`);
-                if (response.status) {
-                    responsesList.push(response);
-                }
-            } catch (error) {
-                console.error(error);
+                responsesList.push(response);
+                pageCount++;
+                isNextPageExists = await response.data.next != null ? true : false;
             }
+            return responsesList;
+        } catch (error) {
+            console.error(error);
         }
-        return responsesList;
     }
-
-
-
 
     async function getPlanetName(planetURL) {
         try {
@@ -43,8 +39,8 @@ export function useAxiosUsers() {
     async function refineUsersDB() {
         try {
             const allUsersRefinedData = [];
-            const usersRawDataEachPage = await getEachPageUsers();
-            usersRawDataEachPage.forEach(rawUser => {
+            const allUsersRawData = await getAllPagesUsers();
+            allUsersRawData.forEach(rawUser => {
                 Object.values(rawUser.data.results).forEach(user => {
                     const userRefinedData = [];
                     for (const [key, value] of Object.entries(user)) {
@@ -57,7 +53,6 @@ export function useAxiosUsers() {
                                 userRefinedData.push(pName); // this push is working but planet name is not shown in local storage
                             });
                         }
-
                     }
                     allUsersRefinedData.push(userRefinedData);
                 });
@@ -88,33 +83,37 @@ export function useAxiosPlanets() {
 
     const planetItems = ['name', 'diameter', 'climate', 'population'];
 
-    async function getValidPlanets() {
-        const responsesList = [];
-        for (let planetCount = 1; planetCount < 100; planetCount++) {
-            try {
-                const response = await axios.get(`https://swapi.dev/api/planets/${planetCount}`);
-                if (response.status) {
-                    responsesList.push(response);
-                }
-            } catch (error) {
-                console.error(error);
-            }   
+    async function getAllPagesPlanets() {
+        try {
+            const responsesList = [];
+            let pageCount = 1;
+            let isNextPageExists = true;
+            while (isNextPageExists) {
+                const response = await axios.get(`https://swapi.dev/api/planets/?page=${pageCount}`);
+                responsesList.push(response);
+                pageCount++;
+                isNextPageExists = await response.data.next != null ? true : false;
+            }
+            return responsesList;
+        } catch (error) {
+            console.error(error);
         }
-        return responsesList;
     }
 
     async function refinePlanetsDB() {
         try {
             const allPlanetsRefinedData = [];            
-            const planetsRawData = await getValidPlanets();
-            planetsRawData.forEach(planet => {
-                const planetRefinedData = [];
-                for (const [key, value] of Object.entries(planet.data)) {
-                    if (planetItems.includes(key)) {
-                        planetRefinedData.push(value);
+            const allPlanetsRawData = await getAllPagesPlanets();
+            allPlanetsRawData.forEach(rawPlanet => {
+                Object.values(rawPlanet.data.results).forEach(planet => {
+                    const planetRefinedData = [];
+                    for (const [key, value] of Object.entries(planet)) {
+                        if (planetItems.includes(key)) {
+                            planetRefinedData.push(value);
+                        }
                     }
-                }
-                allPlanetsRefinedData.push(planetRefinedData);
+                    allPlanetsRefinedData.push(planetRefinedData);
+                });
             });
             return allPlanetsRefinedData;
         } catch (error) {

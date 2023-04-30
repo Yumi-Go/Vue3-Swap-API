c15a5e5da756da70470fe43ddb9e1f3dcbbea08e
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-04-30 20:28:54 +0100

    planet popup working

---
+++ src/components/PlanetPopup.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { Modal } from 'flowbite-vue'
+
+const props = defineProps({
+    planetName: String,
+    planetDiameter: String,
+    planetClimate: String,
+    planetPopulation: String,
+});
+
+const emit = defineEmits([
+    'closeModal'
+]);
+
+</script>
+
+
+
+<template>
+
+<Modal :size="size" @close="$emit('closeModal')">
+    <template #header>
+        <div class="flex items-center text-lg">
+          Homeworld
+        </div>
+    </template>
+    <template #body>
+        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            Planet Name: {{ planetName }}
+        </p>
+        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            Planet Diameter: {{ planetDiameter }}
+        </p>
+        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            Planet Climate: {{ planetClimate }}
+        </p>
+        <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
+            Planet Population: {{ planetPopulation }}
+        </p>
+    </template>
+    <template #footer>
+        <div class="flex justify-between">
+            <!-- <button @click="$emit('closeModal')" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">
+                Close
+            </button> -->
+            <button @click="$emit('closeModal')" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Close
+            </button>
+        </div>
+    </template>
+</Modal>
+
+</template>
+
+
+
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,42 +1,75 @@
 <script setup>
 
-import { onBeforeMount, ref } from 'vue'
+import { onBeforeMount, onMounted, ref } from 'vue'
 import { useFetchData } from '../composables/useFetch';
+import PlanetPopup from './PlanetPopup.vue';
+
 const { fetchData } = useFetchData();
-
 const allData = ref([]);
-
 const personItems = ['No', 'Name', 'Height', 'Mass', 'Created', 'Edited', 'Planet Name'];
+
+const isModalOpened = ref(false);
+const planetName = ref('');
+const planetDiameter = ref('');
+const planetClimate = ref('');
+const planetPopulation = ref('');
 
 onBeforeMount(async () => {
     allData.value = await fetchData();
     console.log("allData: ", allData.value);
+})
 
-});
+function closeModal() {
+    isModalOpened.value = false;
+}
+
+function openModal(pName, pDiameter, pClimate, pPopulation) {
+    planetName.value = pName;
+    planetDiameter.value = pDiameter;
+    planetClimate.value = pClimate;
+    planetPopulation.value = pPopulation;
+    isModalOpened.value = true;
+}
+
 
 </script>
 
 <template>
-<div class="">
-<table class="w-[600px] table-auto border-solid border-2">
-    <thead class="border-solid border-2">
-        <tr class="border-solid border-2">
-            <th v-for="column in personItems" class="border-solid border-2">{{ column }}</th>
-        </tr>
-    </thead>
-    <tbody class="border-solid border-2">
-        <tr v-for="(person, index) in allData" :key="index" class="border-solid border-2">
-            <td class="border-solid border-2">{{ index + 1 }}</td>
-            <td class="border-solid border-2">{{ person.name }}</td>
-            <td class="border-solid border-2">{{ person.height }}</td>
-            <td class="border-solid border-2">{{ person.mass }}</td>
-            <td class="border-solid border-2">{{ person.created }}</td>
-            <td class="border-solid border-2">{{ person.edited }}</td>
-            <td class="border-solid border-2"><span @click="">{{ person.homeworld.name }}</span></td>
 
-        </tr>
-    </tbody>
-</table>
+<div class="">
+
+    <PlanetPopup v-if="isModalOpened"
+    :planetName="planetName"
+    :planetDiameter="planetDiameter"
+    :planetClimate="planetClimate"
+    :planetPopulation="planetPopulation"
+    @closeModal="closeModal"/>
+
+    <table class="w-[600px] table-auto border-solid border-2">
+        <thead class="border-solid border-2">
+            <tr class="border-solid border-2">
+                <th v-for="column in personItems" class="border-solid border-2">{{ column }}</th>
+            </tr>
+        </thead>
+        <tbody class="border-solid border-2">
+            <tr v-for="(person, index) in allData" :key="index" class="border-solid border-2">
+                <td class="border-solid border-2">{{ index + 1 }}</td>
+                <td class="border-solid border-2">{{ person.name }}</td>
+                <td class="border-solid border-2">{{ person.height }}</td>
+                <td class="border-solid border-2">{{ person.mass }}</td>
+                <td class="border-solid border-2">{{ person.created }}</td>
+                <td class="border-solid border-2">{{ person.edited }}</td>
+                <td class="border-solid border-2">
+                    <button
+                    @click="openModal(person.homeworld.name, person.homeworld.diameter, person.homeworld.climate, person.homeworld.population)"
+                    class="cursor-pointer">
+                        {{ person.homeworld.name }}
+                    </button>
+                </td>
+
+            </tr>
+        </tbody>
+    </table>
 </div>
 
 <div class="flex flex-row">

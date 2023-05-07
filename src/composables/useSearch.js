a4744d78a2b7dd1780a3cbaf091b43bcb563522a
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-05-07 19:59:25 +0100

    fix duplicated results in multi columns search

--- src/composables/useSearch.js
+++ src/composables/useSearch.js
@@ -15,6 +15,8 @@ const { sortResult } = useSort();
 export function useSearch() {
 
     function filterByColumns(allData) {
+
+        uniqueIndex.value = [];
         
         if (checkedColumns.value.length < 1) {
             filterByName(allData);
@@ -49,44 +51,92 @@ export function useSearch() {
     }
 
     function filterByName(allData) {
-        const result = allData.filter(obj => obj['name'].toLowerCase().match(search.value.toLowerCase()));
+        const result = allData.filter((obj, index) => {
+            if (obj['name'].toLowerCase().match(search.value.toLowerCase())) {
+                if (!(uniqueIndex.value.includes(index))) {
+                    uniqueIndex.value.push(index);
+                    return obj['name'].toLowerCase().match(search.value.toLowerCase());
+                }
+            }
+        });
         searchResult.value = result;
         console.log("searchResult after filterByName: ", searchResult.value);
+        console.log("unique index of person: ", uniqueIndex.value);
         return result;
     }
 
     function filterByHeight(allData) {
-        const result = allData.filter(obj => obj['height'].match(search.value));
+        const result = allData.filter((obj, index) => {
+            if (obj['height'].match(search.value)) {
+                if (!(uniqueIndex.value.includes(index))) {
+                    uniqueIndex.value.push(index);
+                    return obj['height'].match(search.value);
+                }
+            }
+        });
         searchResult.value = result;
         console.log("searchResult after filterByHeight: ", searchResult.value);
+        console.log("unique index of person: ", uniqueIndex.value);
         return result;
     }
 
     function filterByMass(allData) {
-        const result = allData.filter(obj => obj['mass'].match(search.value));
+        const result = allData.filter((obj, index) => {
+            if (obj['mass'].match(search.value)) {
+                if (!(uniqueIndex.value.includes(index))) {
+                    uniqueIndex.value.push(index);
+                    return obj['mass'].match(search.value);
+                }
+            }
+        });
         searchResult.value = result;
         console.log("searchResult after filterByMass: ", searchResult.value);
+        console.log("unique index of person: ", uniqueIndex.value);
         return result;
     }
 
     function filterByCreated(allData) {
-        const result = allData.filter(obj => obj['created'].toLowerCase().match(search.value));
+        const result = allData.filter((obj, index) => {
+            if (obj['created'].toLowerCase().match(search.value.toLowerCase())) {
+                if (!(uniqueIndex.value.includes(index))) {
+                    uniqueIndex.value.push(index);
+                    return obj['created'].toLowerCase().match(search.value.toLowerCase());
+                }
+            }
+        });
         searchResult.value = result;
         console.log("searchResult after filterByMass: ", searchResult.value);
+        console.log("unique index of person: ", uniqueIndex.value);
         return result;
     }
 
     function filterByEdited(allData) {
-        const result = allData.filter(obj => obj['edited'].toLowerCase().match(search.value));
+        const result = allData.filter((obj, index) => {
+            if (obj['edited'].toLowerCase().match(search.value.toLowerCase())) {
+                if (!(uniqueIndex.value.includes(index))) {
+                    uniqueIndex.value.push(index);
+                    return obj['edited'].toLowerCase().match(search.value.toLowerCase());
+                }
+            }
+        });
         searchResult.value = result;
         console.log("searchResult after filterByMass: ", searchResult.value);
+        console.log("unique index of person: ", uniqueIndex.value);
         return result;
     }
 
     function filterByPlanet(allData) {
-        const result = allData.filter(obj => obj['homeworld']['name']?.toLowerCase().match(search.value.toLowerCase()));
+        const result = allData.filter((obj, index) => {
+            if (obj['homeworld']['name'].toLowerCase().match(search.value.toLowerCase())) {
+                if (!(uniqueIndex.value.includes(index))) {
+                    uniqueIndex.value.push(index);
+                    return obj['homeworld']['name'].toLowerCase().match(search.value.toLowerCase());
+                }
+            }
+        });
         searchResult.value = result;
         console.log("searchResult after filterByPlanet: ", searchResult.value);
+        console.log("unique index of person: ", uniqueIndex.value);
         return result;
     }
 

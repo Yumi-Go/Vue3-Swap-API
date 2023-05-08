15b09736393ff52e8e22f86d03d8fdd4cf554b7c
Yumi-Go <99026603+Yumi-Go@users.noreply.github.com>
2023-05-09 00:29:49 +0100

    add scrolling to top

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <script setup>
 import Search from './components/Search.vue';
 import Result from './components/Result.vue';
+
+window.onscroll = () => fromWhereTopBtnVisibile();
+
+function fromWhereTopBtnVisibile() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+function goToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 </script>
 
 <template>
@@ -8,9 +24,6 @@ import Result from './components/Result.vue';
     <h1>Star Wars Application</h1>
     <Search/>
     <Result/>
+    <button @click="goToTop()" id="topBtn">Top</button>
   </div>
 </template>
-
-<style scoped>
-
-</style>
